cbe92bb9c5cc542f6ffc471fb6a24eca61a48246
Karan Singh <karans21.dev@gmail.com>
2025-03-03 15:43:09 +0530

    Complete the website using Tailwind CSS

--- TailwindCSS_in_Parts/dist/script.js
+++ TailwindCSS_in_Parts/dist/script.js
@@ -42,8 +42,20 @@ const line3 = document.getElementById('line3');
 const line4 = document.getElementById('line4');
 
 
-setupIntersectionObserver(line1, true, 0.5);
-setupIntersectionObserver(line2, false, 0.5);
-setupIntersectionObserver(line3, true, 0.5);
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
 
 setupIntersectionObserver(line4, true, 0.8);
+
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach(element => {
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = element.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+    })
+})
